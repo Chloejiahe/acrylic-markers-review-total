56ccbc662cb8f87eadc22b776fea569ff9a1fe9d
Chloe Yu <1053305029@qq.com>
2026-02-12 09:38:59 +0800

    Update VOC分析代码.py

--- VOC分析代码.py
+++ VOC分析代码.py
@@ -341,10 +341,9 @@ FEATURE_DIC = {
                         'online course', 'skillshare class', 'practice sheets', 'worksheets','online community', 'facebook group'],
         },
         '特殊用途': {
-            '正面-专业级表现': ['professional grade', 'artist grade', 'pro grade', 'professional quality', 'artist quality', 'studio grade', 'museum quality', 'for serious artists', 'not student grade','professional results', 'gallery quality results', 'publication quality',
+            '正面-专业级表现': ["finish looks professional", "looks professional", 'professional grade', 'artist grade', 'pro grade', 'professional quality', 'artist quality', 'studio grade', 'museum quality', 'for serious artists', 'not student grade','professional results', 'gallery quality results', 'publication quality',
                        'industry standard', 'lightfast', 'excellent lightfastness', 'high lightfastness rating', 'fade-resistant', 'fade proof', 'archival quality', 'archival ink', 'archival pigment',"professional looking artwork", "professional paint markers", "look extremely professional", "professional-looking results", "publication quality", "long-lasting and archival-quality artwork", "archival-quality artwork"],
-            '负面-非专业级': ['not professional grade', 'not artist grade', 'hobby grade', 'student grade', 'for hobby use only', 'for casual use only', 'not for serious artists',
-                      'feels like a toy', 'not for client work', 'not for commissions','not archival', 'not lightfast', 'more of a toy than a tool',"not for professional art", "not professional grade", "least desired for professional applications", "great for hobbies and amateur use", "perfect for children and casual art", "not for serious artists", "more of a toy than a tool", "not professional quality", "more advanced student", "not a professional artist"],
+            '负面-非专业级': ["not for professional art", "not professional", "not professional grade", "not professional quality", "kids projects not professional", "any professional art", "best for kids projects", "great for hobbies and amateur use", "perfect for children and casual art", "for casual use", "not for serious artists", "am not a professional", "not a professional artist", "novice tasks", "young artists", "good starter set", "good for beginners"],
             '中性-提及专业性': ['professional grade', 'artist grade', 'hobby grade', 'student grade', 'pro grade', 'lightfast', 'lightfastness rating', 'archival quality', 'archival ink', 'museum quality'],
         },
         '性价比': {
@@ -1045,6 +1044,7 @@ if not df.empty:
                     draw_sku_bubble_chart(role_sub, role, f"role_{i}_{sub_name}", role_specific_dims)
         else:
             st.info("🔍 当前筛选条件下暂无足够的机会维度分析数据。")
+
 
 
 
